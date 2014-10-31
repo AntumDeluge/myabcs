0b054dd8d6f6113f93a07e2fbac4a708e5d7a0c7
antumdeluge <antumdeluge@0ac6d012-5a99-49b8-8c6e-943f5da43a89>
2014-10-31 09:07:15 +0000

    Load an SVG image with wxSVG library.

--- trunk/include/window.h
+++ trunk/include/window.h
@@ -2,11 +2,17 @@
 #define MYABCS_WINDOW
 
 #include <wx/wx.h>
+#include <wxSVG/svg.h>
+#include <wxSVG/svgctrl.h>
 
 class MainWindow : public wxFrame {
 public:
     MainWindow(const wxString& title);
+    void setImage(const wxString& image);
 private:
+    wxPanel* bgpanel;
+    wxSVGCtrl* svg;
+    wxStaticBitmap* imgdisplay;
 };
 
 #endif /* MYABCS_WINDOW */
