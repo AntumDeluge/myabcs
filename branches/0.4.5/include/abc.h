1546927f49794af838900d2d915d170bd764b585
antumdeluge <antumdeluge@0ac6d012-5a99-49b8-8c6e-943f5da43a89>
2014-11-05 21:16:59 +0000

    Use an install prefix for loading images & sounds.

--- branches/0.4.5/include/abc.h
+++ branches/0.4.5/include/abc.h
@@ -23,27 +23,27 @@ const int ID_SPACE = wxNewId(); // Id used in threads for space
 const int ID_KEY = wxNewId(); // Id used in threads for keypress
 const int ID_OTHER = wxNewId(); // Id used in other threads
 
-class MainWindow : public wxFrame
-{
+class MainWindow : public wxFrame {
   public:
     MainWindow(const wxString& title);
     void SetMode(wxCommandEvent& event);
     void OnKey(wxKeyEvent& event);
-    void OnTab();//wxCommandEvent& event);
+    void OnTab(); //wxCommandEvent& event);
     void OnKeyUp(wxKeyEvent& event);
     void GoABC();
     void GoOther();
     void ChangeLetter(wxCommandEvent& event);
     void PlaySound();
-	void OnHelp(wxMenuEvent& event);
-	void OnAbout(wxMenuEvent& event);
-	void EnableAll(wxCommandEvent& event);
-	void SetCurSound(wxString wav);
-	void OnFrameFocus(wxFocusEvent& event); // Redirect focus to main panel
+    void OnHelp(wxMenuEvent& event);
+    void OnAbout(wxMenuEvent& event);
+    void EnableAll(wxCommandEvent& event);
+    void SetCurSound(wxString wav);
+    void OnFrameFocus(wxFocusEvent& event); // Redirect focus to main panel
   private:
     wxString *exedir; // Directory where executable is located
+    wxString installdir; // Directory where files should be installed
     wxToolBar *menu;
-	wxStatusBar *status;
+    wxStatusBar *status;
     wxPanel *bg;
     char key; // Store the key pressed
     char cur_letter;
@@ -59,7 +59,7 @@ class MainWindow : public wxFrame
     wxString animals[2][26];
     wxString musics[2][26];
     wxString toys[2][26];
-//    wxButton *play; // Button to play sounds
+    //    wxButton *play; // Button to play sounds
     wxSound *cheer;
     bool gameend;
     bool isplaying; // Is sound currently playing
@@ -77,7 +77,7 @@ class MainWindow : public wxFrame
     //static void *TabThread(void *arg);
     static void *SpaceThread(void *arg);
     static void *KeyThread(void *arg);
-	static void *OtherThread(void *arg);
+    static void *OtherThread(void *arg);
     pthread_t thread1;
     pthread_t thread2;
     int rc;
