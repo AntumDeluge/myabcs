03c8bab52990f786990e2978defb5dc827fc2fd0
Jordan Irwin <antumdeluge@gmail.com>
2014-11-07 07:20:08 +0000

    Add functions for handling data files.

---
+++ include/data.h
@@ -0,0 +1,36 @@
+#ifndef MYABCS_DATA_H
+#define	MYABCS_DATA_H
+
+#include <string>
+
+/**
+ * 
+ * @return
+ *      Directory string where data files are located
+ */
+const std::wstring getDirData();
+
+/**
+ * 
+ * @return
+ *      Directory string where audio files are located
+ */
+const std::wstring getDirAudio();
+
+/**
+ * 
+ * @return
+ *      Directory string where image files are located
+ */
+const std::wstring getDirImage();
+
+/**
+ * 
+ * @return
+ *      Directory string where logo files are located
+ */
+const std::wstring getDirLogo();
+
+
+#endif	/* MYABCS_DATA_H */
+
