1a563e2cd5fce9d2346d564098a04dbc07543ecd
antumdeluge <antumdeluge@fd847854-fd92-4869-9482-259162b69566>
2014-11-27 22:21:11 +0000

    Add content to index file.

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+$resources = "resource";
+$pixmaps = "$resources/pixmap";
+$favicon = "$pixmaps/favicon.png";
+
+print("
+<html>
+<head>
+<title>MyABCs</title>
+<link rel=\"icon\" type=\"image/png\" href=\"$favicon\" />
+</head>
+
+<body>
+<center><a href=\"https://sourceforge.net/projects/myabcs\">
+Sourceforge Page</a>
+</center>
+
+</body>
+</html>");
