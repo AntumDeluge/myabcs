94e0970cc4cf9d383c264036292273857f144da7
AntumDeluge <antumdeluge@gmail.com>
2016-09-11 18:57:18 -0700

    Categories source & header.

---
+++ src/category.cpp
@@ -0,0 +1,181 @@
+/*
+  The MIT License (MIT)
+
+  Copyright © 2010-2016 Jordan Irwin <antumdeluge@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+  of the Software, and to permit persons to whom the Software is furnished to do
+  so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+
+#include "category.h"
+
+
+const std::vector<std::string> categories {
+	"main",
+	"animal",
+	"food",
+	"music",
+	"toy",
+};
+
+const std::vector<std::string> cat::main {
+	"airplane",
+	"bicycle",
+	"clock",
+	"dolphin",
+	"earth",
+	"fire",
+	"grasshopper",
+	"hat",
+	"igloo",
+	"jar",
+	"keyboard",
+	"ladybug",
+	"magnifying_glass",
+	"newspaper",
+	"onion",
+	"paper_airplane",
+	"quarter",
+	"raccoon",
+	"star",
+	"train",
+	"umbrella",
+	"vase",
+	"watch",
+	"x-ray",
+	"yin_yang",
+	"zipper",
+};
+
+const std::vector<std::string> cat::food {
+	"apple",
+	"bananas",
+	"cherries",
+	"doughnut",
+	"egg",
+	"fries",
+	"grapes",
+	"hot_dog",
+	"ice_cream",
+	"juice",
+	"ketchup",
+	"lasagna",
+	"mushroom",
+	"noodle",
+	"olive",
+	"pretzel",
+	"quiche",
+	"radishes",
+	"strawberry",
+	"tomato",
+	"udon",
+	"vegetables",
+	"watermelon",
+	"x-shaped cookie",
+	"yam",
+	"zucchini",
+};
+
+const std::vector<std::string> cat::animal {
+	"angelfish",
+	"bird",
+	"cat",
+	"dog",
+	"elephant",
+	"frog",
+	"giraffe",
+	"hippo",
+	"horse",
+	"iguana",
+	"jaguar",
+	"koala",
+	"lion",
+	"mouse",
+	"newt",
+	"owl",
+	"pig",
+	"quail",
+	"rooster",
+	"sheep",
+	"turtle",
+	"urchin",
+	"vulture",
+	"whale",
+	"xiphias",
+	"yak",
+	"zebra",
+};
+
+const std::vector<std::string> cat::music {
+	"accordion",
+	"bagpipes",
+	"clarinet",
+	"drums",
+	"electric_guitar",
+	"flute", // Creative Commons
+	"guitar",
+	"harp",
+	"ipu",
+	"jug",
+	"kazoo",
+	"lute",
+	"maracas",
+	"ney",
+	"oboe",
+	"piano",
+	"quena",
+	"recorder",
+	"sax",
+	"trumpet",
+	"ukulele",
+	"violin",
+	"whistle",
+	"xun",
+	"yangqin", // ???
+	"zurna",
+};
+
+const std::vector<std::string> cat::toy {
+	"anklet",
+	"balloons",
+	"chalkboard",
+	"doll",
+	"easel",
+	"football",
+	"glove",
+	"horn",
+	"ice_skate",
+	"jump_rope",
+	"kite",
+	"lunchbox",
+	"magnet",
+	"necklace",
+	"origami",
+	"paint",
+	"quilt",
+	"rocking_horse",
+	"seesaw",
+	"top",
+	"unicycle", // GNU Free Documentation License
+	"volleyball",
+	"wagon",
+	"xylophone",
+	"yoyo",
+	"zipline",
+};
