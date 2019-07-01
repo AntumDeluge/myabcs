c8386e146042aa253b33f32dbeb2e7c4144264b2
Jordan Irwin <antumdeluge@gmail.com>
2019-07-01 01:07:40 -0700

    Delete obsolete MainWindow methods & members

--- include/abc.h
+++ include/abc.h
@@ -2,28 +2,18 @@
 #define MYABCS_ABC_H
 
 #include "imgdisplay.h"
-#include "res.h"
 #include "resourcelist.h"
-#include "resourceobject.h"
 
-#include <wx/wx.h>
-#include <wx/sound.h>
-#include <wx/stdpaths.h>
-#include <wx/filename.h>
-#include <pthread.h>
-#include <fstream>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/statbmp.h>
+#include <wx/stattext.h>
 
 
 class MainWindow : public wxFrame {
 public:
 	MainWindow();
 	void LoadCategory(wxString cat_name);
-	void GoABC();
-	void GoOther();
-	void ChangeLetter(wxCommandEvent& event);
-	void PlaySound();
-	void EnableAll(wxCommandEvent& event);
-	void SetCurSound(wxString wav);
 private:
 	// main GUI elements
 	wxToolBar *menu;
@@ -38,43 +28,12 @@ private:
 	// directories
 	wxString installdir; // Directory where files should be installed
 
-	char key; // Store the key pressed
-	char cur_letter;
-	
 	// game state
+	wxString cur_category; // currently loaded category
 	bool game_end;
-	
-	static wxString alpha_sound;
-	wxString letter_sounds[2][26]; // wav sound for each letter
-	wxString images[26];
-	wxString letters[26];
-	wxString labels[26];
-	wxString foods[2][26];
-	wxString animals[2][26];
-	wxString musics[2][26];
-	wxString toys[2][26];
-	//    wxButton *play; // Button to play sounds
-	bool isplaying; // Is sound currently playing
-	bool canspace; // Allow playing sounds if one is not currently played
-	bool cankey;
 
 	// cached objects for category
 	ResourceList resourceList;
-
-	// Help Dialog
-	//wxDialog *help;
-
-	// About Dialog
-	//GenericAbout *about;
-
-	// Threading
-	//static void *TabThread(void *arg);
-	static void* SpaceThread(void* arg);
-	static void* KeyThread(void* arg);
-	static void* OtherThread(void* arg);
-	pthread_t thread1;
-	pthread_t thread2;
-	int rc;
 
 	// methods
 
