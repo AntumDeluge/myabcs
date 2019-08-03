33ea28b078538f8882c22dd08443b9d80a13e137
Jordan Irwin <antumdeluge@gmail.com>
2019-08-02 19:41:01 -0700

    Add configuration for building built-in freetype library

--- libraries/build.sh
+++ libraries/build.sh
@@ -134,7 +134,7 @@ function extract_archive {
 
 
 # library names in build order
-BUILTIN_LIBS="pkg-config zlib libpng libiconv gettext libffi glib libexif expat harfbuzz libogg libvorbis libflac SDL2 libmpg123 SDL2_mixer wxWidgets wxSVG"
+BUILTIN_LIBS="pkg-config zlib libpng libiconv gettext libffi glib libexif expat harfbuzz freetype libogg libvorbis libflac SDL2 libmpg123 SDL2_mixer wxWidgets wxSVG"
 
 for NAME in ${BUILTIN_LIBS}; do
 	echo -e "\nProcessing ${NAME} ..."
