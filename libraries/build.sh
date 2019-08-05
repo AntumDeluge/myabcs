8d1b3bf1fd55222d756c18536875c2620fb4abb9
Jordan Irwin <antumdeluge@gmail.com>
2019-08-05 14:42:50 -0700

    Use bash shell for library build scripts

--- libraries/build.sh
+++ libraries/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # script to build "built-in" library dependencies
 
--- libraries/list_options.sh
+++ libraries/list_options.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SCRIPT_NAME="$(basename $0)"
 ROOT_DIR="$(dirname $0)/../"
