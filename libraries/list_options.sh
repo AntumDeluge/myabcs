4de600d77f083d50cbe3b8d81e6d6f4c8e2b26e4
Jordan Irwin <antumdeluge@gmail.com>
2019-08-15 18:33:17 -0700

    list_options script: put code into show_lib_options function

--- libraries/list_options.sh
+++ libraries/list_options.sh
@@ -19,6 +19,41 @@ function show_available_libs {
 	fi
 }
 
+function show_lib_options {
+	local target_dir=$1
+	if test -z "${target_dir}"; then
+		echo -e "\nERROR: Target directory not specified in show_lib_options function"
+		exit 1
+	fi
+	local lib_name=$(basename "${target_dir}")
+	cd "${target_dir}"
+	if test -f "configure"; then
+		echo -e "\n${lib_name}: Found GNU Autotools configure script"
+		./configure --help
+	elif test -f "CMakeLists.txt"; then
+		echo -e "\n${lib_name}: Found CMake configuration"
+		tmp_dir="tmp-0000000001"
+		mkdir "${tmp_dir}"
+		cd "${tmp_dir}"
+		cmake ../ > /dev/null 2>&1
+		cmake -LA ../ | awk '{if(f)print} /-- Cache values/{f=1}'
+		cd ../
+		rm -r "${tmp_dir}"
+	elif test -f "meson.build"; then
+		if test ! -f "meson_options.txt"; then
+			echo -e "\n${lib_name}: ERROR: Found Meson Build configuration, but \"meson_options.txt\" is missing"
+			exit 1
+		fi
+		echo -e "\n${lib_name}: Found Meson Build configuration"
+		cat "meson_options.txt"
+	else
+		echo -e "\nERROR: No recognizable configuration found in ${target_dir}"
+		exit 1
+	fi
+
+	cd "${LIBS_DIR}"
+}
+
 LIB_NAME=$1
 if test -z "${LIB_NAME}"; then
 	echo -e "\nPlease supply library directory name (usually as <name>-<version>): ${SCRIPT_NAME} <directory>"
@@ -33,29 +68,4 @@ if test ! -d "${TARGET_DIR}"; then
 	exit 1
 fi
 
-cd "${TARGET_DIR}"
-if test -f "configure"; then
-	echo -e "\n${LIB_NAME}: Found GNU Autotools configure script"
-	./configure --help
-elif test -f "CMakeLists.txt"; then
-	echo -e "\n${LIB_NAME}: Found CMake configuration"
-	tmp_dir="tmp-0000000001"
-	mkdir "${tmp_dir}"
-	cd "${tmp_dir}"
-	cmake ../ > /dev/null 2>&1
-	cmake -LA ../ | awk '{if(f)print} /-- Cache values/{f=1}'
-	cd ../
-	rm -r "${tmp_dir}"
-elif test -f "meson.build"; then
-	if test ! -f "meson_options.txt"; then
-		echo -e "\n${LIB_NAME}: ERROR: Found Meson Build configuration, but \"meson_options.txt\" is missing"
-		exit 1
-	fi
-	echo -e "\n${LIB_NAME}: Found Meson Build configuration"
-	cat "meson_options.txt"
-else
-	echo -e "\nERROR: No recognizable configuration found in ${TARGET_DIR}"
-	exit 1
-fi
-
-cd "${LIBS_DIR}"
+show_lib_options "${TARGET_DIR}"
