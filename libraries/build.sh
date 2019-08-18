8072c5454f782144a53e558b2bad5414f1601f0b
Jordan Irwin <antumdeluge@gmail.com>
2019-08-18 13:36:05 -0700

    Add config for internal xorg-util-macros

--- libraries/build.sh
+++ libraries/build.sh
@@ -175,7 +175,7 @@ if ${OS_WIN}; then
 	OPTIONAL_NO_DEPENDS+=" winpthreads"
 fi
 
-NO_DEPENDS="zlib bzip2 expat graphite2 libffi libogg"
+NO_DEPENDS="zlib bzip2 expat graphite2 libffi libogg xorg-util-macros"
 if ${INCLUDE_OPTIONAL}; then
 	NO_DEPENDS="${OPTIONAL_NO_DEPENDS} ${NO_DEPENDS}"
 fi
