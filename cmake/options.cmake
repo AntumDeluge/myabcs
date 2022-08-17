be9c185c047b1356e76dcdfd7841f49cc447f26a
Jordan Irwin <antumdeluge@gmail.com>
2022-08-17 00:49:06 -0700

    CMake: move some config options into submodule

---
+++ cmake/options.cmake
@@ -0,0 +1,5 @@
+
+# app is portable by default
+set(PORTABLE ON CACHE BOOL "App searches for data files in path relative to executable")
+
+set(USE_BUNDLED OFF CACHE BOOL "Prioritize bundled libs over system ones")
