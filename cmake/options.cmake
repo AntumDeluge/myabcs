7d7288afe4bec7da78b0cde9cd3f60eb18d2d6d6
Jordan Irwin <antumdeluge@gmail.com>
2022-08-17 15:08:18 -0700

    CMake: use DEBUG internally only

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,4 +1,17 @@
 
+set(DEBUG ON) # for use internally instead of typing "CMAKE_BUILD_TYPE STREQUAL"
+if(NOT CMAKE_BUILD_TYPE STREQUAL Release)
+	set(CMAKE_BUILD_TYPE Debug CACHE STRING "Build type" FORCE)
+	add_compile_definitions(DEBUG=1)
+else()
+	set(DEBUG OFF)
+	set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type" FORCE)
+endif()
+set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS Release Debug)
+
+# strip binary for release builds
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -s")
+
 # app is portable by default
 set(PORTABLE ON CACHE BOOL "App searches for data files in path relative to executable")
 
