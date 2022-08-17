338bae9074e83e01f971efd0200ef04ac8516161
Jordan Irwin <antumdeluge@gmail.com>
2022-08-17 01:08:35 -0700

    CMake: add option to link statically to libgcc & libstdc++

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,3 +3,8 @@
 set(PORTABLE ON CACHE BOOL "App searches for data files in path relative to executable")
 
 set(USE_BUNDLED OFF CACHE BOOL "Prioritize bundled libs over system ones")
+
+set(STATIC OFF CACHE BOOL "Link statically to libgcc & libstdc++")
+if(STATIC)
+	set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
+endif()
